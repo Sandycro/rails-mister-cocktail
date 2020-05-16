6e7070c2da9cc7335825479994af5d1664879a46
sandy smircic <sandy.smircic@hotmail.com>
2020-05-16 22:21:04 +0100

    Changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts 'Cleaning database...'
-Ingredient.destroy_all
-
-puts 'Creating Ingredients...'
-
-["lemon", "ice", "mint leaves"].each do |attribute|
-  ingredient = Ingredient.create(name: attribute)
-  puts "Created! #{ingredient.name}"
-end
-puts 'Finished!'
 
 puts 'Cleaning database...'
 Cocktail.destroy_all
@@ -24,3 +14,21 @@ Cocktail.create(name: "Mint Julep")
 Cocktail.create(name: "Bloody Mary")
 Cocktail.create(name: "Whiskey Sour")
 Cocktail.create(name: "Caipirinha")
+
+require "open-uri"
+
+file = URI.open('https://res.cloudinary.com/dxrby0f3r/image/upload/v1589644937/timur-romanov-PSj-v1o2s28-unsplash_y9xutx.jpg')
+cocktail = Cocktail.new(title: 'NES', body: "A great console")
+cocktail.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating Ingredient...'
+
+["lemon", "ice", "mint leaves"].each do |attribute|
+  ingredient = Ingredient.create(name: attribute)
+  puts "Created! #{ingredient.name}"
+end
+puts 'Finished!'
+
