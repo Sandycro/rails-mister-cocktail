5ff9efc0ed46e2725fb4ca8a30a922ac242dcdc4
sandy smircic <sandy.smircic@hotmail.com>
2020-05-24 11:16:42 +0100

    Changes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,28 +8,17 @@ class CocktailsController < ApplicationController
   end
 
   def new
-    @cocktail = Cocktail.new # needed to instantiate the form_for
+    @cocktail = Cocktail.new
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-  #no need for app/views/cocktails/create.html.erb
       redirect_to cocktail_path(@cocktail)
     else
       render 'new'
     end
   end
-
-  # def edit
-  #   @cocktail = Cocktail.find(params[:id])
-  # end
-
-  # def update
-  #   @cocktail = Cocktail.find(params[:id])
-  #   @cocktail.update(cocktail_params)
-  #   redirect_to cocktail_path(@cocktail)
-  # end
 
   def destroy
     @cocktail = Cocktail.find(params[:id])
